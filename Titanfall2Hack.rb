0823a046135a2c8faecd434aa6de99bdc069a26c
owerfanflypeople13 <giuliaer3834@gmail.com>
2025-06-20 16:21:43 -0400

    Add files via upload

---
+++ Titanfall2Hack.rb
@@ -0,0 +1,5 @@
+# CBLYQEtZO7
+def method_QmqxD
+    puts "Hello from function 6efcdcfca3a643cd9cebd72eaf6df2c0_20250619080305828817!"
+end
+method_QmqxD
---
+++ Titanfall2HackTrainer.rs
@@ -0,0 +1,4 @@
+// CBLYQEtZO7
+fn main() {
+    println!("Hello from function 6efcdcfca3a643cd9cebd72eaf6df2c0_20250619080305828817!");
+}
